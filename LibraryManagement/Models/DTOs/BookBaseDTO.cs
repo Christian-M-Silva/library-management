fe8a721eb211f44f13570bb7831d0389a7a64a4f
Christian Silva <tininjacms@gmail.com>
2025-05-26 16:06:30 -0300

    feat(RequestBookUpdate): Add o model de RequestBookUpdate

--- LibraryManagement/Models/DTOs/BookBaseDTO.cs
+++ LibraryManagement/Models/DTOs/BookBaseDTO.cs
@@ -1,9 +1,9 @@
-﻿namespace LibraryManagement.DTOs
+﻿namespace LibraryManagement.Models.DTOs
 {
     public class BookBaseDTO
     {
-        public string Title { get; set; } = String.Empty;
-        public string Author { get; set; } = String.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
         public int PublishYear { get; set; }
         public string? Gender { get; set; }
         public bool IsRemoved { get; set; }
---
+++ LibraryManagement/Models/Requests/RequestBookUpdate.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Models.Requests
+{
+    public class RequestBookUpdate
+    {
+        public string? Title { get; set; } = string.Empty;
+        public string? Author { get; set; } = string.Empty;
+        public int? PublishYear { get; set; }
+        public string? Gender { get; set; }
+    }
+}
