a0c1e19ba06042a428700d7fd323fdf0156b5f6b
Christian Silva <tininjacms@gmail.com>
2025-05-26 16:02:29 -0300

    feat(BookBaseDTO): Criará DTO do bookBase

---
+++ LibraryManagement/DTOs/BookBaseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryManagement.DTOs
+{
+    public class BookBaseDTO
+    {
+        public string Title { get; set; } = String.Empty;
+        public string Author { get; set; } = String.Empty;
+        public int PublishYear { get; set; }
+        public string? Gender { get; set; }
+        public bool IsRemoved { get; set; }
+    }
+}
