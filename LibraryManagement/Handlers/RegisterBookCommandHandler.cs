c370559f64c9843bc76f17fb1f7524e6ee9120e4
Christian Silva <tininjacms@gmail.com>
2025-05-28 11:43:16 -0300

    feat(handlerRegister): Criará o handler de registrar o livro

---
+++ LibraryManagement/Handlers/RegisterBookCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using LibraryManagement.Commands;
+using LibraryManagement.Models.Entities;
+using MediatR;
+
+namespace LibraryManagement.Handlers
+{
+    public class RegisterBookCommandHandler(MyDbContext dbContext) : IRequestHandler<RegisterBookCommand, Guid>
+    {
+		private readonly MyDbContext _dbContext = dbContext;
+
+        public async Task<Guid> Handle(RegisterBookCommand request, CancellationToken cancellationToken)
+        {
+			try
+			{
+				Guid id = Guid.NewGuid();
+				BookEntity book = new()
+				{
+					Id = id,
+					IsRemoved = false,
+					Author = request.Author,
+					Gender = request.Gender,
+					PublishYear = request.PublishYear,
+					Title = request.Title
+                };
+				_dbContext.Books.Add(book);
+				await _dbContext.SaveChangesAsync(cancellationToken);
+				return id;
+			}
+			catch (Exception err)
+			{
+				throw new Exception(err.Message);
+			}
+        }
+    }
+}
