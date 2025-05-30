d73a4c0e4621ff8a7b1d04dd0ae66789eb28a77e
Christian Silva <tininjacms@gmail.com>
2025-05-30 10:26:26 -0300

    feat(AddControllerBook): Add controller de book e finaliza a funcionalidade de registrar livro

---
+++ LibraryManagement/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManagement.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> RegisterBook(RegisterBookCommand requestBook) {
+
+            try
+            {
+                Guid id = await _mediator.Send(requestBook);
+                return StatusCode(201, id);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+    }
+}
