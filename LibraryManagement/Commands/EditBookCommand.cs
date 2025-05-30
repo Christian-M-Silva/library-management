d89c8ba057e4f4426fd6b9d8c8faca88fee9375f
Christian Silva <tininjacms@gmail.com>
2025-05-30 10:30:43 -0300

    feat(EditBookCommand): Criará o comando de editar o livro

---
+++ LibraryManagement/Commands/EditBookCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Commands
+{
+    public class EditBookCommand
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public int? PublishYear { get; set; }
+        public string? Gender { get; set; }
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
