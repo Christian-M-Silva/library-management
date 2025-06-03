253ed63b193f01cd6d91bccfad9ded19db7ac482
Christian Silva <tininjacms@gmail.com>
2025-06-03 08:57:12 -0300

    feat(logicaGetBook): Criará a query e o handle de pegar o livro

---
+++ LibraryManagement/Handlers/GetBookQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagement.Exceptions;
+using LibraryManagement.Models.Entities;
+using LibraryManagement.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Handlers
+{
+    public class GetBookQueryHandler(MyDbContext dbContext) : IRequestHandler<GetBookQuery, BookEntity>
+    {
+        private readonly MyDbContext _dbContext = dbContext;
+        public async Task<BookEntity> Handle(GetBookQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                BookEntity? book = await _dbContext.Books.FirstOrDefaultAsync(bookEntity => bookEntity.Id == request.Id, cancellationToken: cancellationToken);
+
+                return book ?? throw new NotFoundException("book not found");
+            }
+            catch (Exception err)
+            {
+                throw new Exception(err.Message);
+            }
+        }
+    }
+}
---
+++ LibraryManagement/Queries/GetBookQuery.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagement.Models.Entities;
+using MediatR;
+
+namespace LibraryManagement.Queries
+{
+    public class GetBookQuery: IRequest<BookEntity>
+    {
+        public Guid Id { get; set; }
+    }
+}
